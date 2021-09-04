e3b014bd144de563bf7da78c1907f261d2fa0912
Patrick_Cin <patrickphatng@gmail.com>
2021-09-04 16:52:38 +0700

    Revert "Update final code: 6 nets + modeling + tuning + inference"

--- models/embeddingnet.py
+++ models/embeddingnet.py
@@ -1,117 +1,18 @@
 from torch import nn
-import pytorch_lightning as pl
 
-class EmbeddingNet(pl.LightningModule):
+class EmbeddingNet(nn.Module):
     '''
     Multilayer Perceptron.
     '''
-    def __init__(self,input_dim, dropout=0.25):
+    def __init__(self,input_dim):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
+            nn.Dropout(),
             nn.Linear(input_dim, 64),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(64, 128),
             nn.ReLU(),
-            nn.Linear(64, 64)
-        )
-    def forward(self, x):
-        '''
-        Forward pass
-        '''
-        return self.layers(x)
-
-class EmbeddingNetMedium(pl.LightningModule):
-    '''
-    Multilayer Perceptron.
-    '''
-    def __init__(self,input_dim, dropout=0.25):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32)
-        )
-    def forward(self, x):
-        '''
-        Forward pass
-        '''
-        return self.layers(x)
-
-class EmbeddingNetSmall(pl.LightningModule):
-    '''
-    Multilayer Perceptron.
-    '''
-    def __init__(self,input_dim, dropout=0.25):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32)
-        )
-    def forward(self, x):
-        '''
-        Forward pass
-        '''
-        return self.layers(x)
-
-class EmbeddingNetTiny(pl.LightningModule):
-    '''
-    Multilayer Perceptron.
-    '''
-    def __init__(self,input_dim, dropout=0.25):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16)
-        )
-    def forward(self, x):
-        '''
-        Forward pass
-        '''
-        return self.layers(x)
-
-class EmbeddingNetSuperTiny(pl.LightningModule):
-    '''
-    Multilayer Perceptron.
-    '''
-    def __init__(self,input_dim, dropout=0.25):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 16),
-            nn.ReLU(),
-            nn.Linear(16, 16),
-            nn.ReLU(),
-            nn.Linear(16, 8)
-        )
-    def forward(self, x):
-        '''
-        Forward pass
-        '''
-        return self.layers(x)
-    
-class EmbeddingNetAtom(pl.LightningModule):
-    '''
-    Multilayer Perceptron.
-    '''
-    def __init__(self,input_dim, dropout=0.25):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 16),
-            nn.ReLU(),
-            nn.Linear(16, 8)
+            nn.Linear(128, 64)
         )
     def forward(self, x):
         '''
--- models/tripletnet.py
+++ models/tripletnet.py
@@ -3,37 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch.autograd import Variable
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class TripletNet(pl.LightningModule):
-    def __init__(self, embeddingnet, use_scheduler:False):
-        super().__init__()
+    def __init__(self, embeddingnet):
+        super(TripletNet, self).__init__()
         self.embeddingnet = embeddingnet
-        self.use_scheduler = use_scheduler
         self.criterion = torch.nn.MarginRankingLoss(0.2)
 
     def forward(self, x, y, z):
-        embedded_x = self.embeddingnet(x.float())
-        embedded_y = self.embeddingnet(y.float())
-        embedded_z = self.embeddingnet(z.float())
+        embedded_x = self.embeddingnet(x)
+        embedded_y = self.embeddingnet(y)
+        embedded_z = self.embeddingnet(z)
         dist_a = F.pairwise_distance(embedded_x, embedded_y, 2)
         dist_b = F.pairwise_distance(embedded_x, embedded_z, 2)
         return dist_a, dist_b, embedded_x, embedded_y, embedded_z
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        if self.use_scheduler:
-            scheduler = ReduceLROnPlateau(optimizer, patience=5)
-            return {"optimizer": optimizer, "scheduler": scheduler, "monitor": "train_loss"}
-        else: 
-            return optimizer
+        return optimizer
 
     def forward_loss(self, x,y,z):
+        
         dist_a, dist_b, embedded_x, embedded_y, embedded_z = self.forward(x,y,z)
         target = torch.FloatTensor(dist_a.size()).fill_(1)
-        target = Variable(target).to(self.device)
-        loss_triplet = self.criterion(dist_a, dist_b, target)
+        target = Variable(target)
+        loss_triplet = criterion(dist_a, dist_b, target)
         loss_embedd = embedded_x.norm(2) + embedded_y.norm(2) + embedded_z.norm(2)
         loss = loss_triplet + 0.001 * loss_embedd
         return loss
@@ -41,11 +36,9 @@ class TripletNet(pl.LightningModule):
     def training_step(self, batch, idx):
         x, y, z = batch
         loss = self.forward_loss(x,y,z)
-        self.log("train_loss", loss)
         return loss
         
     def validation_step(self, batch, idx):
         x, y, z = batch
         loss = self.forward_loss(x,y,z)
-        self.log("val_loss", loss)
         return loss
